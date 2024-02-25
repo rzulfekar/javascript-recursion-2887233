6ddd7141f0339cfcc6ccd1e19808265026cc554a
Rex Zulfekar <rzulfekar@users.noreply.github.com>
2024-02-25 10:33:14 -0500

    added recursive implementation

--- palindrome_iteration.js
+++ palindrome_iteration.js
@@ -1,16 +1,32 @@
 
-function isPalindrome(phrase){
+function isPalindrome(phrase) {
 	const size = phrase.length
 
 	for (let i = 0; i < size / 2; i++) {
-		if (phrase[i] !== phrase[size - 1 -i]){
-			console.log(`${i} ==> ${phrase[i]} : ${size - 1  - i} ==> ${phrase[size - 1  - i] }`)
+		if (phrase[i] !== phrase[size - 1 - i]) {
+			console.log(`${i} ==> ${phrase[i]} : ${size - 1 - i} ==> ${phrase[size - 1 - i]}`)
 			return false
 		}
 	}
 	return true
 }
 
-console.log("motor: " +  isPalindrome("motor"))
-console.log("rotor: " +  isPalindrome("rotor"))
-console.log("a man a plan a canal panama:" +  isPalindrome("amanaplanacanalpanama"))
+function isPalindromeRecursive(phrase) {
+	if (phrase.length <= 1) return true;
+
+	if (phrase[0] === phrase[phrase.length - 1]) {
+		// abba
+		return isPalindromeRecursive(phrase.slice(1, phrase.length - 1));
+	}
+
+	return false;
+}
+
+console.log('********** iteratively *************')
+console.log("motor: " + isPalindrome("motor"))
+console.log("rotor: " + isPalindrome("rotor"))
+console.log("a man a plan a canal panama:" + isPalindrome("amanaplanacanalpanama"))
+console.log('********** recursively *************')
+console.log("motor: " + isPalindromeRecursive("motor"))
+console.log("rotor: " + isPalindromeRecursive("rotor"))
+console.log("a man a plan a canal panama:" + isPalindromeRecursive("amanaplanacanalpanama"))
